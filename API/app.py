b273df66971567ff838275e2a290138479e87353
Дмитрий Берестнев <1135@zolotoykod.ru>
2021-10-27 19:25:17 +0400

    fix

--- API/app.py
+++ API/app.py
@@ -16,4 +16,4 @@ def unarhive(projectId=None):
     return make_response('', 204)
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True,host='0.0.0.0', port=8090)
